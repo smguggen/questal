07c00697221dbd9de1caf7824c5e8fa063710d7c
smguggen <steven@directmail.io>
2020-04-06 08:59:45 -0500

    Testing requestal integration

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const Requestal = require('requestal');
+const Questal =  require('./src/questal');
+const Request;
+if (typeof exports === 'object' && typeof module !== 'undefined') {
+    Request = Requestal
+} else if (typeof window === 'object' && typeof XMLHttpRequest === 'function') {
+    Request = Questal;
+} else {
+    throw new Error('Questal is not supported in this environment');
+}
+
+exports.client = Questal;
+exports.server = Requestal;
 module.exports = require('./src/questal');
+
+
